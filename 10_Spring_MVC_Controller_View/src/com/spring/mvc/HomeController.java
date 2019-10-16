845fb6cf68839b451bbe2a40bad9870a787da2d0
Rapter <sngermiyanoglu@hotmail.com>
2019-10-16 10:42:00 +0300

    Add files via upload

---
+++ 10_Spring_MVC_Controller_View/src/com/spring/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+}
